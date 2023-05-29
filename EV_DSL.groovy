2eb489ce940d0b7eb33980bb8bb7a1e67ba3ffc3
girish.mohite <gim090182@gmail.com>
2023-05-29 17:37:46 +0530

    EV DSL file modified

--- EV_DSL.groovy
+++ EV_DSL.groovy
@@ -7,7 +7,7 @@ job("Environment_Variables_DSL") {
 		shell("""echo "Current build no. - 					\$BUILD_ID"
 echo "Current build is accessible at - 		\$BUILD_URL"
 echo "Java library is available at - 		\$JAVA_HOME"
-echo "Jenkins workspace is available at - 	\$WORKSPACE"
+echo "Jenkins workspace is available at - 	\$WORKSPACE")
 	}
 	wrappers {
 		timestamps()
