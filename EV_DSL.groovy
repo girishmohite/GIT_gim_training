d1468f7f891983743b350a37a8d2168de76cbee2
girish.mohite <gim090182@gmail.com>
2023-05-29 17:24:23 +0530

    Added EV DSL file

---
+++ EV_DSL.groovy
@@ -0,0 +1,30 @@
+job("Environment_Variables_DSL") {
+	description("This job is for understanding Jenkins environment variables.")
+	keepDependencies(false)
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		shell("""echo "Current build no. - 					\$BUILD_ID"
+echo "Current build is accessible at - 		\$BUILD_URL"
+echo "Java library is available at - 		\$JAVA_HOME"
+echo "Jenkins workspace is available at - 	\$WORKSPACE"
+
+echo "##################################################"
+
+echo "System Admin Name is - 				\$SYSTEM_ADMIN_NAME"
+echo "System Admin Email is - 				\$SYSTEM_ADMIN_EMAIL"""")
+	}
+	wrappers {
+		timestamps()
+	}
+	configure {
+		it / 'properties' / 'jenkins.model.BuildDiscarderProperty' {
+			strategy {
+				'daysToKeep'('-1')
+				'numToKeep'('5')
+				'artifactDaysToKeep'('-1')
+				'artifactNumToKeep'('-1')
+			}
+		}
+	}
+}
